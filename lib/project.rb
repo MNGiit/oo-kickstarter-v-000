deb94d815015987ba5b4d880625aea0c5a7f10e0
Mohammed Noori <kmanoorib@gmail.com>
2018-06-24 21:21:16 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,7 @@ class Project
   
   def add_backer (backer)
     @backers << backer
-    backer.back_project(self) if !Backer.all.include?(backer)
+    backer.back_project(self) if !Backer.all.name.include?(backer)
   end
+
 end
